41bd1b58011d8c40a58593de07a8c27ed2a54acf
Kevin <83800548+graezykev@users.noreply.github.com>
2024-05-17 09:52:41 +0000

    demos

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Server 1 ==============================================================
+
 const http = require('http');
 
 const hostname = '0.0.0.0';
@@ -6,9 +8,23 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, World Node.js!\n');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// Server 2 ==============================================================
+
+const express = require('express')
+const app = express()
+const port2 = 8081
+
+app.get('/', (req, res) => {
+  res.send('Hello World Express!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port2}`)
+})
