390496c60969c582ec88b58d1fd72ec46073f4aa
Kev <chunwen.yang@shopee.com>
2024-05-20 15:01:06 +0800

    test postgresql

--- index.js
+++ index.js
@@ -17,12 +17,35 @@ server.listen(port1, hostname, () => {
 
 // Server 2 ==============================================================
 
+const { Client } = require('pg')
 const express = require('express')
 const app = express()
 const port2 = 8081
 
 app.get('/', (req, res) => {
-  res.send('Hello World Express!')
+
+  // `psql -h postgres -U postgres -d postgres`
+  const client = new Client({
+    host: 'postgres',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+  })
+
+  client.connect()
+    .then(() => {
+      console.log('Connected to PostgreSQL')
+      res.send('Hello World Express! - PostgreSQL Connection Succeeds!')
+    })
+    .catch(err => {
+      console.error('Connection error', err.stack)
+      res.send('Hello World Express! - PostgreSQL Connection Fails!')
+    })
+    .finally(() => {
+      console.log('Disconnected to PostgreSQL')
+      client.end()
+    })
 })
 
 app.listen(port2, () => {
